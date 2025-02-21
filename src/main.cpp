6e4d8db0004c417e741b9d5487019c70843c3531
jjelo <jelinek@outlook.sk>
2025-02-21 23:54:49 +0100

    ATC_52DF98 test

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,12 @@ JsonDocument sensorData;
 
 static constexpr uint32_t scanTimeMs = 10000; 
 
+
+int16_t hexStringToInt16(const char* hexString) {
+  char* endptr;
+  return (int16_t)strtol(hexString, &endptr, 16);
+}
+
 class scanCallbacks : public NimBLEScanCallbacks 
 {
   void onDiscovered(const NimBLEAdvertisedDevice* advertisedDevice) override { }
@@ -59,7 +65,7 @@ class scanCallbacks : public NimBLEScanCallbacks
 
     std::string serviceData = BLEdata["servicedata"];
 
-    if(BLEdata["name"] == "SBHT-003C" && serviceData.length() == 20 || serviceData.length() == 24)
+    if(BLEdata["name"] == "SBHT-003C" && (serviceData.length() == 20 || serviceData.length() == 24))
     {
       JsonObject shellyHT = sensorData.to<JsonObject>();
 
@@ -73,42 +79,51 @@ class scanCallbacks : public NimBLEScanCallbacks
       uint8_t lowByte = (uint8_t)strtoul(serviceData.substr(serviceData.length()-4, 2).c_str(), NULL, 16);
       uint8_t highByte = (uint8_t)strtoul(serviceData.substr(serviceData.length()-2, 2).c_str(), NULL, 16);
       uint8_t bytes[] = {lowByte, highByte};
-      String t_str = String(*(int16_t*)bytes);
-      int16_t temp_c = (int16_t)strtol(t_str.c_str(), NULL, 16);
-      shellyHT["temperature_c"] = (float)temp_c/100;
+      int16_t temp_c = (int16_t)strtol(String(*(int16_t*)bytes).c_str(), NULL, 10);
+      shellyHT["temp_c"] = (float)temp_c/10;
+      shellyHT["temp_f"] = (temp_c * 18 + 3200) / (float)100;
       
       if(serviceData.length() == 24 && serviceData.substr(16, 4) == "0145") shellyHT["button"] = 1;
       else if (serviceData.length() == 24 && serviceData.substr(16, 4) == "fe45") shellyHT["button"] = 2;
       else shellyHT["button"] = 0;
 
+      shellyHT["service_data"] = serviceData;
+
       serializeJson(shellyHT, Serial);
       Serial.println();
     }
 
-    if(BLEdata["name"] == "ESP" && serviceData.length() == 32)
+    else if(BLEdata["name"] == "ESP" && serviceData.length() == 32)
     {
       JsonObject espHTP = sensorData.to<JsonObject>(); 
 
       espHTP["mac"] = mac_adress;
       espHTP["name"] = "ESP32 HTP";
-  
-      uint32_t pressure = strtoul(serviceData.substr(8, 8).c_str(), NULL, 16);
-      espHTP["pressure"] = (float)pressure/100;
-
-      int16_t humidity = (int16_t)strtol(serviceData.substr(16, 4).c_str(), NULL, 16);
-      espHTP["humidity"] = (float)humidity/100;
-
-      int16_t temp_c = (int16_t)strtol(serviceData.substr(20, 4).c_str(), NULL, 16);
-      espHTP["temp_c"] = (float)temp_c/100;
-
-      espHTP["temp_f"] = (temp_c * 18 + 32000)/ (float)1000;
-
-      espHTP["packet"] = strtoul(serviceData.substr(24, 8).c_str(), NULL, 16);
-
+      espHTP["pressure"] = (uint32_t)strtoul(serviceData.substr(8, 8).c_str(), NULL, 16)/(float)100;
+      espHTP["humidity"] = (int16_t)strtol(serviceData.substr(16, 4).c_str(), NULL, 16) / (float)100;
+      espHTP["temp_c"] = (int16_t)strtol(serviceData.substr(20, 4).c_str(), NULL, 16) / (float)100;
+      espHTP["temp_f"] = roundf(((int16_t)strtol(serviceData.substr(20, 4).c_str(), NULL, 16) * 18 + 32000) / 1000.0 * 100.0) / 100.0;
+      espHTP["packet"] = (uint32_t)strtoul(serviceData.substr(24, 8).c_str(), NULL, 16);
       espHTP["service_data"] = serviceData;
 
       serializeJson(espHTP, Serial);
       Serial.println( );       
+    }
+    else if(BLEdata["name"] == "ATC_52DF98" && serviceData.length() == 26)
+    {
+      JsonObject miaHT = sensorData.to<JsonObject>(); 
+
+      miaHT["mac"] = mac_adress;
+      miaHT["name"] = "ATC_52DF98";
+      miaHT["temp_c"] = (int16_t)strtol(serviceData.substr(12, 4).c_str(), NULL, 16) / (float)10;
+      miaHT["humidity"] = (uint8_t)strtoul(serviceData.substr(16, 2).c_str(), NULL, 16);
+      miaHT["battery_p"] = (uint8_t)strtoul(serviceData.substr(18, 2).c_str(), NULL, 16);
+      miaHT["battery_v"] = (uint16_t)strtoul(serviceData.substr(20, 4).c_str(), NULL, 16) / (float)1000;
+      miaHT["packet"] = (uint8_t)strtoul(serviceData.substr(24, 2).c_str(), NULL, 16);
+      miaHT["service_data"] = serviceData;
+
+      serializeJson(miaHT, Serial);
+      Serial.println();
     }
   }
 
@@ -124,12 +139,20 @@ void setup()
   NimBLEDevice::init("ESP32-Scanner");    
   NimBLEScan* pBLEScan = NimBLEDevice::getScan(); 
 
-  /*
-    NimBLEAddress sensor1("AA:BB:CC:DD:EE:FF", BLE_ADDR_PUBLIC);
-    NimBLEDevice::whiteListAdd(sensor1);
-    
-    pBLEScan->setFilterPolicy(BLE_HCI_SCAN_FILT_USE_WL);
-  */
+
+
+  NimBLEAddress sensor1("7C:C6:B6:72:A0:5C", BLE_ADDR_PUBLIC);
+  NimBLEDevice::whiteListAdd(sensor1);
+
+  NimBLEAddress sensor2("AC:15:18:E4:DD:F2", BLE_ADDR_PUBLIC);
+  NimBLEDevice::whiteListAdd(sensor2);
+
+  NimBLEAddress sensor3("A4:C1:38:52:DF:98", BLE_ADDR_PUBLIC);
+  NimBLEDevice::whiteListAdd(sensor3);
+  
+  pBLEScan->setFilterPolicy(BLE_HCI_SCAN_FILT_USE_WL);
+  
+
 
   pBLEScan->setScanCallbacks(&scanCallbacks, false);
   pBLEScan->setActiveScan(true);         
