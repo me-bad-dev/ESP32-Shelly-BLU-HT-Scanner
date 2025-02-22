25fa9bb654163f98ae6c8548e074981348a0fc93
me-bad-dev <44929407+me-bad-dev@users.noreply.github.com>
2025-02-22 19:50:18 +0100

    Update main.cpp
    Added "rssi" to JSON output and removed unnecessary code used for testing.

--- src/main.cpp
+++ src/main.cpp
@@ -7,13 +7,7 @@ NimBLEScan* pBLEScan;
 JsonDocument bleScanData;
 JsonDocument sensorData;
 
-static constexpr uint32_t scanTimeMs = 10000; 
-
-
-int16_t hexStringToInt16(const char* hexString) {
-  char* endptr;
-  return (int16_t)strtol(hexString, &endptr, 16);
-}
+static constexpr uint32_t scanTime_ms = 10000; 
 
 class scanCallbacks : public NimBLEScanCallbacks 
 {
@@ -65,13 +59,14 @@ class scanCallbacks : public NimBLEScanCallbacks
 
     std::string serviceData = BLEdata["servicedata"];
 
-    if(BLEdata["name"] == "SBHT-003C" && (serviceData.length() == 20 || serviceData.length() == 24))
+    if(BLEdata["servicedatauuid"] == "0xfcd2" && (serviceData.length() == 20 || serviceData.length() == 24))
     {
       JsonObject shellyHT = sensorData.to<JsonObject>();
 
       shellyHT["mac"] = mac_adress;
+      shellyHT["rssi"] = (int)advertisedDevice->getRSSI();
       shellyHT["name"] = "ShellyBLU H&T";
-      shellyHT["model"] = "SBHT-003C";
+      shellyHT["model"] = (char*)advertisedDevice->getName().c_str();
       shellyHT["packet"] = (uint8_t)strtoul(serviceData.substr(4, 2).c_str(), NULL, 16);
       shellyHT["battery"] = (uint8_t)strtoul(serviceData.substr(8, 2).c_str(), NULL, 16);
       shellyHT["humidity"] = (uint8_t)strtoul(serviceData.substr(12, 2).c_str(), NULL, 16);
@@ -92,44 +87,20 @@ class scanCallbacks : public NimBLEScanCallbacks
       serializeJson(shellyHT, Serial);
       Serial.println();
     }
-
-    else if(BLEdata["name"] == "ESP" && serviceData.length() == 32)
+    else
     {
-      JsonObject espHTP = sensorData.to<JsonObject>(); 
+      /* Uncomment to print data to serial from other deivces */
 
-      espHTP["mac"] = mac_adress;
-      espHTP["name"] = "ESP32 HTP";
-      espHTP["pressure"] = (uint32_t)strtoul(serviceData.substr(8, 8).c_str(), NULL, 16)/(float)100;
-      espHTP["humidity"] = (int16_t)strtol(serviceData.substr(16, 4).c_str(), NULL, 16) / (float)100;
-      espHTP["temp_c"] = (int16_t)strtol(serviceData.substr(20, 4).c_str(), NULL, 16) / (float)100;
-      espHTP["temp_f"] = roundf(((int16_t)strtol(serviceData.substr(20, 4).c_str(), NULL, 16) * 18 + 32000) / 1000.0 * 100.0) / 100.0;
-      espHTP["packet"] = (uint32_t)strtoul(serviceData.substr(24, 8).c_str(), NULL, 16);
-      espHTP["service_data"] = serviceData;
-
-      serializeJson(espHTP, Serial);
-      Serial.println( );       
-    }
-    else if(BLEdata["name"] == "ATC_52DF98" && serviceData.length() == 26)
-    {
-      JsonObject miaHT = sensorData.to<JsonObject>(); 
-
-      miaHT["mac"] = mac_adress;
-      miaHT["name"] = "ATC_52DF98";
-      miaHT["temp_c"] = (int16_t)strtol(serviceData.substr(12, 4).c_str(), NULL, 16) / (float)10;
-      miaHT["humidity"] = (uint8_t)strtoul(serviceData.substr(16, 2).c_str(), NULL, 16);
-      miaHT["battery_p"] = (uint8_t)strtoul(serviceData.substr(18, 2).c_str(), NULL, 16);
-      miaHT["battery_v"] = (uint16_t)strtoul(serviceData.substr(20, 4).c_str(), NULL, 16) / (float)1000;
-      miaHT["packet"] = (uint8_t)strtoul(serviceData.substr(24, 2).c_str(), NULL, 16);
-      miaHT["service_data"] = serviceData;
-
-      serializeJson(miaHT, Serial);
-      Serial.println();
+      /*
+        serializeJson(BLEdata, Serial);
+        Serial.println();
+      */
     }
   }
 
   void onScanEnd(const NimBLEScanResults& results, int reason) override 
   {
-      NimBLEDevice::getScan()->start(scanTimeMs, false, true);
+      NimBLEDevice::getScan()->start(scanTime_ms, false, true);
   }
 
 } scanCallbacks;
@@ -139,25 +110,22 @@ void setup()
   NimBLEDevice::init("ESP32-Scanner");    
   NimBLEScan* pBLEScan = NimBLEDevice::getScan(); 
 
-
-
-  NimBLEAddress sensor1("7C:C6:B6:72:A0:5C", BLE_ADDR_PUBLIC);
+/* 
+    Obtaining data from Shelly BLUE H&T requres active scanning.
+    It is recommended to whitelist our sensors so we dont request
+    scan responses from other devices to reduce taffic.
+*/
+/*
+  NimBLEAddress sensor1("AA:BB:CC:DD:EE:FF", BLE_ADDR_PUBLIC);
   NimBLEDevice::whiteListAdd(sensor1);
-
-  NimBLEAddress sensor2("AC:15:18:E4:DD:F2", BLE_ADDR_PUBLIC);
-  NimBLEDevice::whiteListAdd(sensor2);
-
-  NimBLEAddress sensor3("A4:C1:38:52:DF:98", BLE_ADDR_PUBLIC);
-  NimBLEDevice::whiteListAdd(sensor3);
   
   pBLEScan->setFilterPolicy(BLE_HCI_SCAN_FILT_USE_WL);
-  
-
+*/
 
   pBLEScan->setScanCallbacks(&scanCallbacks, false);
   pBLEScan->setActiveScan(true);         
   pBLEScan->setMaxResults(0);            
-  pBLEScan->start(scanTimeMs, false, true);
+  pBLEScan->start(scanTime_ms, false, true);
 
   Serial.begin(115200);
 
